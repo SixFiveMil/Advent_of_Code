f2afae228357f6cd0d776dd73fafc24b048cb80f
Joshua Wortz <50379453+SixFiveMil@users.noreply.github.com>
2024-11-30 18:14:32 -0800

    Finished Day 1

--- 2015/Day 1/d01-hard.py
+++ 2015/Day 1/d01-hard.py
@@ -1,13 +1,20 @@
 #read input file in directory where the script is saved at
 import os
 r = 0
+p = 0
 with open('2015\Day 1\input.txt', 'r') as f:
     for x in str(f.readlines()):
-        print(x)
+#find char position when r = -1
+
+        #print(x)
+        if r == -1:
+            continue
         if x == "(":
             r+=1
+            p+=1
         elif x== ")":
             r-=1
+            p+=1
 
-print(r)
+print(r, p)
 
